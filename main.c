4ff69f3dce942f83c48aad2aa6a58111ea464621
Zack <zphixon@gmail.com>
2022-05-10 13:45:23 -0400

    annotate mian.c

--- main.c
+++ main.c
@@ -1,17 +1,27 @@
 #include <assert.h>
 #include <dlfcn.h>
 
+// just include your class header
 #include "object.h"
 
 int main() {
-    object obj; make_object(&obj);
+    // make an instance of your class
+    object obj;
+    make_object(&obj);
 
+    // send it messages (call its methods)
     obj.setX(3);
     debug_object(&obj);
     assert(obj.getX() == 3);
 
-    object obj2; make_object(&obj2);
+    // maybe make another one?
+    object obj2;
+    make_object(&obj2);
+
+    // do something different
     obj2.setX(4);
     debug_object(&obj2);
+
+    // as expected
     assert(obj.getX() != obj2.getX());
 }
